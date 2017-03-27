b5793b6e95c1889c6541fe44c609a1c329acc6f6
Sam Haycraft <shaycraft@metlife.com>
2017-03-27 13:32:08 -0600

    buffered reading

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import stdout
 import urllib2
 from lxml import html
 import os
@@ -5,6 +6,7 @@ import os
 url = "http://www2.census.gov/geo/tiger/TIGER2015/TRACT"
 htmlStr = urllib2.urlopen(url).read()
 DOWNLOAD_FOLDER = 'Downloads'
+BUFFER_SIZE = 300 * 1024
 
 if not os.path.exists(DOWNLOAD_FOLDER):
     print 'Creating downloads folder...'
@@ -14,6 +16,14 @@ zips = html.fromstring(htmlStr).xpath('//table/tr/td/a[last()]/text()')[1:]
 for z in zips:
     with open('Downloads/{0}'.format(z), 'wb') as download_file:
         downloadUrl = '{0}/{1}'.format(url, z)
-        print 'Downloading {0}...'.format(downloadUrl)
-        download_file.write(urllib2.urlopen(downloadUrl).read())
+        print 'Downloading {0}'.format(downloadUrl)
+        res = urllib2.urlopen(downloadUrl)
+        while True:
+            chunk = res.read(BUFFER_SIZE)
+            if not chunk:
+                break
+            download_file.write(chunk)
+            print '.',
+    print
+
 
